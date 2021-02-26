9deb529134ef22cc645f8e3f827af169813b383c
mich3ll3e <michelle.ouyang@outlook.com>
2021-02-26 13:44:56 -0500

    fixed bugs

--- seeds.sql
+++ seeds.sql
@@ -18,8 +18,21 @@ VALUES("George", "Hill", 1, NULL)
 , ("Eric", "Wade", 12, 11);
 
 
-INSERT INTO roles (titile, salary, department_id)
-VALUES ("Sales Lead", 50000, 1),("Sales Person", 30000, 1), ("Legal Team Lead", 90000, 4);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("CEO", 250000, 1)
+, ("COO", 200000, 1) 
+, ("CTO", 200000, 5) 
+, ("CFO", 200000, 3)
+, ("Sales Team Lead", 125000, 2) 
+, ("Salesperson", 90000, 2)
+, ("Controller", 100000, 3)
+, ("Accountant", 800000, 3)
+, ("Legal Team Lead", 150000, 4) 
+, ("Lawyer", 110000, 4) 
+, ("Engineer Team Lead", 135000, 5) 
+, ("Software Engineer", 95000, 5);
 
 INSERT INTO department (name)
 VALUES ("Sales"),("Enginering"), ("Finance"),("Legal");
+SELECT * FROM roles;
